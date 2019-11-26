16c568edfc37651315a5cba03416392b66718a25
MariyaEvseeva <45750137+MariyaEvseeva@users.noreply.github.com>
2019-11-27 01:54:12 +0300

    Update основа.py
    Добавлена функция транскрипции (без особенностей произношения, в общем виде)

--- основа.py
+++ основа.py
@@ -50,7 +50,54 @@ class Example(QWidget):
         self.label4.setText("МЕСТО ДЛЯ АУДИОФАЙЛА")
 
     def transcription(self):
-        self.answer = self.word_input.text()[::-1]
+        # self.answer = self.word_input.text()[::-1]
+        # self.label1.setText(self.answer)
+        word = self.word_input.text()
+        word = word.lower()
+
+        d ={
+            "а": "а", "б": ["б", "б'"], "в": ["в", "в'"], "г": ["г", "г'"],
+            "д": ["д", "д'"], "е": ["э", "й'э"], "ё": ["о", "й'о"], "ж": "ж",
+            "з": ["з", "з'"], "и": "и", "й": "й'", "к": ["к", "к'"], "л": ["л", "л'"],
+            "м": ["м", "м'"], "н": ["н", "н'"], "о": "о", "п": ["п", "п'"],
+            "р": ["р", "р'"], "с": ["с", "с'"], "т": ["т", "т'"], "у": "у",
+            "ф": ["ф", "ф'"], "х": ["х", "х'"], "ц": "ц", "ч": "ч'", "ш": "ш",
+            "щ": "щ'", "ы": "ы", "э": "э",  "ю": ["у", "й'у"],
+            "я": ["а", "й'а"]
+            }
+    
+        consonants = 'йцкнгшщзхждлрпвфчсмтб'
+        vowels = 'ёуеэоаыяию'
+        smth = 'ъь'
+    
+        trnscrpt = '['
+        for i in range(len(word)):
+            if word[i] in d:
+                w = word[i]
+        
+                if w in vowels:
+                    if w in 'уэоаыи':
+                        trnscrpt += w
+                    elif w in 'ёеяю':
+                        if i == 0:
+                            trnscrpt += d[w][1]
+                        else:
+                            if word[i - 1] in vowels or word[i - 1] in smth:
+                                trnscrpt += d[w][1]
+                            else:
+                                trnscrpt += d[w][0]
+    
+                if w in consonants:
+                    if w in 'йцшщжч':
+                        trnscrpt += d[w]
+                    else:
+                        if i != len(word)-1 and word[i + 1] in 'ёеяиью':
+                            trnscrpt += d[w][1]
+                        else:
+                            trnscrpt += d[w][0]
+    
+        trnscrpt += ']'
+        self.answer = trnscrpt
         self.label1.setText(self.answer)
 
     def syllable(self):
